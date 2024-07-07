cf5f61e901f7e2db7661b66223506c7136f0f51d
Abhishek <134132023+Abhisss221@users.noreply.github.com>
2024-07-08 02:03:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("app.py")) # as soon as our application will be initialized our api ki will be loaded
+## Function to load OpenAI model and get respones
+model = genai.GenerativeModel('gemini-pro')
+def get_gemini_response(question):
+    
+    response = model.generate_content(question)
+    return response.text
+##initialize our streamlit app
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Agile phase_1  : LLM1 -> testing ->: next scrum -> finetuning, tokenization   ")
+
+input=st.text_input("Input: ",key="input")
+submit=st.button("Ask the question")
+## If ask button is clicked
+
+if submit:
+    
+    response=get_gemini_response(input)
+    st.subheader("The Response is")
+    st.write(response)
